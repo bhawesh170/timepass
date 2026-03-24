696be8e260b20dd8dc62fa3a5306dea7f289c336
bhawesh170 <bhaweshkumarsharma@gmail.com>
2026-03-24 23:07:03 +0530

    Refactor button click handler and update alerts

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let main = document.querySelector(".container");
 let input = document.getElementById("nameInput");
 let btn = document.querySelector("button");
+let body = document.body;
 
 btn.addEventListener("click", function() {
-    let name = input.value;
-    if (name !== "gadhi") {
-        alert("गधी ! जो बुलाता हूँ वो नाम डालो ।");
-    } else {
+    let name = input.value.trim().toLowerCase();
+    if (name === "gadhi") {
+        body.style.backgroundImage = "url(ravi.jpg.jpeg)";
         main.style.display = "none";
-        main.style.backgroundImg = url("ravi.jpg");
-        alert("मुझसे शादी करोगी ?");
+        document.querySelector(".result").style.display = "block";
+        document.querySelector(".result").textContent = "मेरे बेटवा से शादी कर के जल्दी आ जाओ । नहीं तो मैं भी तुम्हें गधी ही बुलाऊँगा ।";
+    } else {
+        alert("गधी ! जो बुलाता हूँ वो नाम डालो ।");
     }
 });
